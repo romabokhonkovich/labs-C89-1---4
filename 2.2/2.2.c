da94da0a95a7c2cd2037d5eda72ab25637ef583a
romabokhonkovich <bokhonkovich2015@mail.ru>
2020-04-18 20:43:30 +0300

    remake

--- 2.2/2.2.c
+++ 2.2/2.2.c
@@ -1,14 +1,14 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
+﻿#include<stdio.h>
 #include<math.h>
+#include<windows.h>
 
 int main()
 {
 	double x, e;
 	printf("x =  ");
-	scanf_s("%lf", &x);
+	scanf("%lf", &x);
 	printf("e = ");
-	scanf_s("%lf", &e);
+	scanf("%lf", &e);
 	double result = x;
 	double sum = x;
 
@@ -19,10 +19,9 @@ int main()
 		result = result * (-1) * pow(x, 2) / ((2 * i) * (2 * i + 1));
 	    sum += result;
 	    i += 1;
-		if (i > 100 || !isfinite(sum))
+		if (i > 100)
 		{
 			printf("uncorrect enter");
-			prin
 			return 0;
 		}
     } 
@@ -34,5 +33,6 @@ int main()
 	 printf("\ne = %f", e);
 	 printf("\n|sin(x) - sum| = %f",fabs(sin(x) - sum));
 	 printf("\n");
+         return 0;
 }
 
